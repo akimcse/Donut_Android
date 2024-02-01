24ffc98daedd1d3e047794d490a28b0d4fd4d54c
hyuna <akxmcse@gmail.com>
2024-02-02 02:23:52 +0900

    [feat] progress bar 구현

--- app/src/main/java/org/gdsc/donut/ui/home/GiverHomeFragment.kt
+++ app/src/main/java/org/gdsc/donut/ui/home/GiverHomeFragment.kt
@@ -1,14 +1,18 @@
 package org.gdsc.donut.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import org.gdsc.donut.databinding.FragmentGiverHomeBinding
+import kotlin.properties.Delegates
 
 class GiverHomeFragment : Fragment() {
     private lateinit var binding: FragmentGiverHomeBinding
+    private var donatedAmount by Delegates.notNull<Double>()
+    private var requiredAmount by Delegates.notNull<Double>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,12 +20,39 @@ class GiverHomeFragment : Fragment() {
     ): View? {
         binding = FragmentGiverHomeBinding.inflate(inflater, container, false)
 
-        //함수 호출
+        setAmount()
+        setProgressBar()
 
         return binding.root
     }
 
-    //함수 작성
+    private fun setAmount(){
+        // 나중에 서버에서 값 받아오기
+        donatedAmount = 200.0
+        requiredAmount = 1000.0
+    }
+
+    private fun setProgressBar(){
+        val percentage = (donatedAmount/requiredAmount*100)
+        binding.progressBar.progress = percentage.toInt()
+        if(percentage>=0 && percentage<20){
+            setProgressBarPointer(300)
+        } else if(percentage>=20 && percentage<40){
+            setProgressBarPointer(340)
+        } else if(percentage>=40 && percentage<80){
+            setProgressBarPointer(350)
+        } else if(percentage>=80){
+            setProgressBarPointer(355)
+        }
+    }
+
+    private fun setProgressBarPointer(ratio: Int){
+        val percentage = (donatedAmount/requiredAmount*100)
+        val percentage360 = percentage/100*ratio
+        binding.tvPercentNum.translationX = percentage360.toFloat()
+        binding.tvPercent.translationX = percentage360.toFloat()
+        binding.ivPointer.translationX = percentage360.toFloat()
+    }
 
     companion object {
     }
