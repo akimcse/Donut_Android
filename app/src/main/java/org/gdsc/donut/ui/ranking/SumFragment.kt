e2735147d135f575d0a491e2ca8ae40e958da58d
hyuna <akxmcse@gmail.com>
2024-02-04 00:38:44 +0900

    [feat] sum fragment 화면 구현

--- app/src/main/java/org/gdsc/donut/ui/ranking/SumFragment.kt
+++ app/src/main/java/org/gdsc/donut/ui/ranking/SumFragment.kt
@@ -5,10 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.gdsc.donut.data.local.SumRankingData
 import org.gdsc.donut.databinding.FragmentSumBinding
+import org.gdsc.donut.ui.ranking.adapter.SumRankingAdapter
 
 class SumFragment : Fragment() {
     private lateinit var binding: FragmentSumBinding
+    private lateinit var itemAdapter: SumRankingAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,12 +21,27 @@ class SumFragment : Fragment() {
     ): View? {
         binding = FragmentSumBinding.inflate(inflater, container, false)
 
-        //함수 호출
+        setAdapter()
 
         return binding.root
     }
 
-    //함수 작성
+    private fun setAdapter() {
+        itemAdapter = SumRankingAdapter()
+        binding.rvRanking.adapter = itemAdapter
+        binding.rvRanking.layoutManager = LinearLayoutManager(context)
+        itemAdapter.itemList.addAll(
+            listOf(
+                SumRankingData(2, "user1", "14,800"),
+                SumRankingData(3, "user2", "1,480"),
+                SumRankingData(4, "user3", "148"),
+                SumRankingData(5, "user4", "14"),
+                SumRankingData(6, "user5", "1"),
+                SumRankingData(7, "user6", "0"),
+                )
+        )
+    }
+
 
     companion object {
     }
