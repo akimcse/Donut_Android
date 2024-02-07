4e1dfffd6c93f98c4959870f4f809fa9775c34df
hyuna <akxmcse@gmail.com>
2024-02-07 09:58:28 +0900

    [feat] 기부 화면 구현 완료

--- app/src/main/java/org/gdsc/donut/ui/donation/DonationFragment.kt
+++ app/src/main/java/org/gdsc/donut/ui/donation/DonationFragment.kt
@@ -1,21 +1,29 @@
 package org.gdsc.donut.ui.donation
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import org.gdsc.donut.R
 import org.gdsc.donut.databinding.FragmentDonationBinding
-import org.gdsc.donut.ui.GiverMainActivity
 
 class DonationFragment : Fragment() {
     private lateinit var binding: FragmentDonationBinding
-    private val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-        if (uri != null) { setImage(uri) }
-    }
+    private val pickMedia =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                setImage(uri)
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +31,73 @@ class DonationFragment : Fragment() {
     ): View? {
         binding = FragmentDonationBinding.inflate(inflater, container, false)
 
+        setEditTextFocus()
+        setStoreList()
         setUploadButton()
+        setDonateButton()
 
         return binding.root
+    }
+
+    // 입력 여부에 따라 색 변하도록 다시 처리
+    private fun setEditTextFocus() {
+        binding.etName.onFocusChangeListener =
+            View.OnFocusChangeListener { _, hasFocus ->
+                if (hasFocus) {
+                    binding.etName.background.setTint(resources.getColor(R.color.main_coral))
+                } else {
+                    binding.etName.background.setTint(resources.getColor(R.color.black_100))
+                }
+            }
+
+        binding.etAmount.onFocusChangeListener =
+            View.OnFocusChangeListener { _, hasFocus ->
+                if (hasFocus) {
+                    binding.etAmount.background.setTint(resources.getColor(R.color.main_coral))
+                } else {
+                    binding.etAmount.background.setTint(resources.getColor(R.color.black_100))
+                }
+            }
+
+        binding.etDue.onFocusChangeListener =
+            View.OnFocusChangeListener { _, hasFocus ->
+                if (hasFocus) {
+                    binding.etDue.background.setTint(resources.getColor(R.color.main_coral))
+                } else {
+                    binding.etDue.background.setTint(resources.getColor(R.color.black_100))
+                }
+            }
+    }
+
+
+    private fun setStoreList() {
+        binding.clStore.setOnClickListener {
+            binding.clStore.visibility = View.GONE
+            binding.clStoreList.visibility = View.VISIBLE
+            binding.tvCu.visibility = View.VISIBLE
+            binding.tvGs25.visibility = View.VISIBLE
+        }
+        setStoreClickListener()
+    }
+
+    private fun setStoreClickListener() {
+        binding.tv7eleven.setOnClickListener {
+            binding.tv7eleven.setTextColor(resources.getColor(R.color.main_coral))
+            binding.tvCu.setTextColor(resources.getColor(R.color.black_100))
+            binding.tvGs25.setTextColor(resources.getColor(R.color.black_100))
+        }
+
+        binding.tvCu.setOnClickListener {
+            binding.tv7eleven.setTextColor(resources.getColor(R.color.black_100))
+            binding.tvCu.setTextColor(resources.getColor(R.color.main_coral))
+            binding.tvGs25.setTextColor(resources.getColor(R.color.black_100))
+        }
+
+        binding.tvGs25.setOnClickListener {
+            binding.tv7eleven.setTextColor(resources.getColor(R.color.black_100))
+            binding.tvCu.setTextColor(resources.getColor(R.color.black_100))
+            binding.tvGs25.setTextColor(resources.getColor(R.color.main_coral))
+        }
     }
 
     private fun setUploadButton() {
@@ -35,22 +107,27 @@ class DonationFragment : Fragment() {
     }
 
     private fun setImage(uri: Uri) {
-        binding.ivGifticon.setImageURI(uri)
+        Glide.with(this)
+            .load(uri)
+            .centerCrop()
+            .into(binding.ivGifticon)
         binding.ivGifticon.visibility = View.VISIBLE
-        binding.btnDonate.visibility = View.VISIBLE
-        setDonateButton()
     }
 
     private fun setDonateButton() {
-        binding.btnDonate.setOnClickListener {
-            getImageInfo()
-            requireActivity().supportFragmentManager.beginTransaction().remove(this).commit()
-            (activity as GiverMainActivity).changeFragment("donation_confirm") // 화면 전환 시 이름, 금액, 기한, 사용처, 사진 uri 담아서 전환
+        // 버튼 예외처리 서버 통신할 때 다시 보기
+        if (!binding.etName.text.isNullOrEmpty() && !binding.etAmount.text.isNullOrEmpty() && !binding.etDue.text.isNullOrEmpty() && binding.clStore.visibility == View.GONE && binding.ivGifticon.visibility == View.VISIBLE) {
+            binding.btnDonate.visibility = View.VISIBLE
+            binding.btnDonate.setOnClickListener {
+                //sendDonationInfo()
+                requireActivity().supportFragmentManager.beginTransaction().remove(this).commit()
+                startActivity(Intent(context, DonationDoneActivity::class.java))
+            }
         }
     }
 
-    private fun getImageInfo() {
-        // OCR 사용해서 정보 추출
+    private fun sendDonationInfo() {
+        // api 리스폰스에 이름, 금액, 기한, 사용처, 사진 담아서 전송
     }
 
     companion object {
