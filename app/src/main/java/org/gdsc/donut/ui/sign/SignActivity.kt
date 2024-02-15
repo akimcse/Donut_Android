f17e8f5c0f0e29203408b8168fdf6bc8d755fbef
hyuna <akxmcse@gmail.com>
2024-02-16 02:56:01 +0900

    [feat] 로그인 정보 입력에 따라 버튼 활성화

--- app/src/main/java/org/gdsc/donut/ui/sign/SignActivity.kt
+++ app/src/main/java/org/gdsc/donut/ui/sign/SignActivity.kt
@@ -2,13 +2,21 @@ package org.gdsc.donut.ui.sign
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import org.gdsc.donut.R
 import org.gdsc.donut.data.DonutSharedPreferences
 import org.gdsc.donut.databinding.ActivitySignBinding
-import org.gdsc.donut.ui.GiverMainActivity
+import org.gdsc.donut.ui.ReceiverMainActivity
+import org.gdsc.donut.ui.viewModel.SignViewModel
 
 class SignActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignBinding
+    private val viewModel: SignViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +24,61 @@ class SignActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        autoLogin()
         setContinueBtn()
+        checkNameStatus()
+        checkPasswordStatus()
     }
 
-    private fun setContinueBtn(){
+
+
+    private fun setContinueBtn() {
         binding.btnCreate.setOnClickListener {
             startActivity(Intent(this, SignUpConfirmActivity::class.java))
         }
     }
+
+    private fun checkNameStatus() {
+        binding.etUsername.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.clUsername.setBackgroundDrawable(getDrawable(R.drawable.bg_white_maincoral_round8))
+                binding.etUsername.setTextColor(getColor(R.color.black_100))
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (binding.etUsername.text.isNullOrBlank()) {
+                    binding.clUsername.setBackgroundDrawable(getDrawable(R.drawable.bg_white_black_round8))
+                    binding.etUsername.setTextColor(getColor(R.color.gray_300))
+                }
+                setLoginBtn()
+            }
+        })
+    }
+
+    private fun checkPasswordStatus() {
+        binding.etPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.clPassword.setBackgroundDrawable(getDrawable(R.drawable.bg_white_maincoral_round8))
+                binding.etPassword.setTextColor(getColor(R.color.black_100))
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (binding.etPassword.text.isNullOrBlank()) {
+                    binding.clPassword.setBackgroundDrawable(getDrawable(R.drawable.bg_white_black_round8))
+                    binding.etPassword.setTextColor(getColor(R.color.gray_300))
+                }
+                setLoginBtn()
+            }
+        })
+    }
+
+
 }
