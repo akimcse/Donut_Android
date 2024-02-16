f845bdcc8510a0cf88811b8d89cef6006423d2da
hyuna <akxmcse@gmail.com>
2024-02-17 02:51:20 +0900

    [feat] receiver home api 기본 정보 연결

--- app/src/main/java/org/gdsc/donut/ui/home/ReceiverHomeFragment.kt
+++ app/src/main/java/org/gdsc/donut/ui/home/ReceiverHomeFragment.kt
@@ -4,16 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
+import org.gdsc.donut.data.DonutSharedPreferences
 import org.gdsc.donut.data.local.PackageItemData
 import org.gdsc.donut.databinding.FragmentReceiverHomeBinding
 import org.gdsc.donut.ui.ReceiverMainActivity
 import org.gdsc.donut.ui.home.adpater.PackageItemAdapter
+import org.gdsc.donut.ui.viewModel.HomeViewModel
+import org.gdsc.donut.ui.viewModel.SignViewModel
 
 class ReceiverHomeFragment : Fragment() {
     private lateinit var binding: FragmentReceiverHomeBinding
     private lateinit var itemAdapter: PackageItemAdapter
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onStart() {
+        super.onStart()
+
+        DonutSharedPreferences.getAccessToken()?.let { viewModel.requestReceiverHomeInfo(it) }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,9 +34,29 @@ class ReceiverHomeFragment : Fragment() {
     ): View? {
         binding = FragmentReceiverHomeBinding.inflate(inflater, container, false)
 
+        getReceiverHomeInfo()
         setAdapter()
 
         return binding.root
+    }
+
+    private fun getReceiverHomeInfo(){
+        viewModel.receiverHomeInfo.observe(viewLifecycleOwner, Observer { data ->
+            if(data.data!!.availability) {
+                binding.tvTitle.visibility = View.VISIBLE
+                binding.imageView4.visibility = View.VISIBLE
+                binding.imageView5.visibility = View.INVISIBLE
+            } else {
+                binding.tvTitle.visibility = View.GONE
+                binding.imageView5.visibility = View.VISIBLE
+                binding.imageView4.visibility = View.INVISIBLE
+            }
+
+            binding.tvDollarNum.text = data.data.amount.toString()
+            binding.tv7Num.text = data.data.sevenEleven.toString()
+            binding.tvCuNum.text = data.data.cu.toString()
+            binding.tvGsNum.text = data.data.gs25.toString()
+        })
     }
 
     private fun setAdapter(){
--- app/src/main/java/org/gdsc/donut/ui/viewModel/HomeViewModel.kt
+++ app/src/main/java/org/gdsc/donut/ui/viewModel/HomeViewModel.kt
@@ -17,15 +17,26 @@ import org.gdsc.donut.data.remote.response.auth.ResponseSignInGiver
 import org.gdsc.donut.data.remote.response.auth.ResponseSignInReceiver
 import org.gdsc.donut.data.remote.response.auth.ResponseSignUpReceiver
 import org.gdsc.donut.data.remote.response.home.ResponseHomeGiver
+import org.gdsc.donut.data.remote.response.home.ResponseHomeReceiver
 
 class HomeViewModel(application: Application) : AndroidViewModel(application) {
     private val _giverHomeInfo = MutableLiveData<ResponseHomeGiver>()
     val giverHomeInfo: LiveData<ResponseHomeGiver>
         get() = _giverHomeInfo
 
-    fun requestGiverSignIn(accessToken: String) = viewModelScope.launch(Dispatchers.IO) {
+    private val _receiverHomeInfo = MutableLiveData<ResponseHomeReceiver>()
+    val receiverHomeInfo: LiveData<ResponseHomeReceiver>
+        get() = _receiverHomeInfo
+
+    fun requestGiverHomeInfo(accessToken: String) = viewModelScope.launch(Dispatchers.IO) {
         _giverHomeInfo.postValue(
             RetrofitBuilder.homeService.getGiverHomeInfo("Bearer $accessToken")
+        )
+    }
+
+    fun requestReceiverHomeInfo(accessToken: String) = viewModelScope.launch(Dispatchers.IO) {
+        _receiverHomeInfo.postValue(
+            RetrofitBuilder.homeService.getReceiverHomeInfo("Bearer $accessToken")
         )
     }
 }
