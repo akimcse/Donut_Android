2dedfc85d0cf6a63507f8a4d23e0cc6d7cd3d780
hyuna <akxmcse@gmail.com>
2024-02-17 01:12:55 +0900

    [feat] giver home api data class 및 interface 추가

--- app/src/main/java/org/gdsc/donut/data/api/HomeService.kt
+++ app/src/main/java/org/gdsc/donut/data/api/HomeService.kt
@@ -1,4 +1,12 @@
 package org.gdsc.donut.data.api
 
+import org.gdsc.donut.data.remote.response.home.ResponseHomeGiver
+import retrofit2.http.GET
+import retrofit2.http.Header
+
 interface HomeService {
+    @GET("home/giver")
+    suspend fun getGiverHomeInfo(
+        @Header("Authorization") accessToken : String
+    ): ResponseHomeGiver
 }
