283bb62c00d2e0f914e1f62d56e972134aa531e4
hyuna <akxmcse@gmail.com>
2024-02-17 02:26:25 +0900

    [feat] receiver home api data 및 interface 추가

--- app/src/main/java/org/gdsc/donut/data/api/HomeService.kt
+++ app/src/main/java/org/gdsc/donut/data/api/HomeService.kt
@@ -1,6 +1,7 @@
 package org.gdsc.donut.data.api
 
 import org.gdsc.donut.data.remote.response.home.ResponseHomeGiver
+import org.gdsc.donut.data.remote.response.home.ResponseHomeReceiver
 import retrofit2.http.GET
 import retrofit2.http.Header
 
@@ -9,4 +10,9 @@ interface HomeService {
     suspend fun getGiverHomeInfo(
         @Header("Authorization") accessToken : String
     ): ResponseHomeGiver
+
+    @GET("home/receiver")
+    suspend fun getReceiverHomeInfo(
+        @Header("Authorization") accessToken : String
+    ): ResponseHomeReceiver
 }
