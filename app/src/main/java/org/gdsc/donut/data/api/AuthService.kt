793a0638f4e7fd1066711603aa0d4f5cbb960530
hyuna <akxmcse@gmail.com>
2024-02-16 17:54:01 +0900

    [chore] Authorization 필드명 수정

--- app/src/main/java/org/gdsc/donut/data/api/AuthService.kt
+++ app/src/main/java/org/gdsc/donut/data/api/AuthService.kt
@@ -22,6 +22,6 @@ interface AuthService {
 
     @POST("auth/giver/signin")
     suspend fun signInGiver(
-        @Header("accesstoken") accessToken : String
+        @Header("Authorization") accessToken : String
     ): ResponseSignInGiver
 }
