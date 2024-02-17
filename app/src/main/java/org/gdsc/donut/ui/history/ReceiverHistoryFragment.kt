d2bc40afa11915012fb82861c1623f3babbab941
hyuna <akxmcse@gmail.com>
2024-02-18 00:14:00 +0900

    [feat] 잔액 정보 연동

--- app/src/main/java/org/gdsc/donut/ui/history/ReceiverHistoryFragment.kt
+++ app/src/main/java/org/gdsc/donut/ui/history/ReceiverHistoryFragment.kt
@@ -5,14 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.google.android.material.tabs.TabLayoutMediator
+import org.gdsc.donut.data.DonutSharedPreferences
 import org.gdsc.donut.databinding.FragmentReceiverHistoryBinding
 import org.gdsc.donut.ui.history.adapter.HistoryViewPagerAdapter
+import org.gdsc.donut.ui.viewModel.HistoryViewModel
 
 class ReceiverHistoryFragment : Fragment() {
     private lateinit var binding: FragmentReceiverHistoryBinding
     private lateinit var itemViewPagerAdapter: HistoryViewPagerAdapter
-
+    private val viewModel: HistoryViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,10 +24,22 @@ class ReceiverHistoryFragment : Fragment() {
     ): View? {
         binding = FragmentReceiverHistoryBinding.inflate(inflater, container, false)
 
+        initNetwork()
+        getReceiverHistoryInfo()
         setViewPager()
         initTabLayout()
 
         return binding.root
+    }
+
+    private fun initNetwork(){
+        DonutSharedPreferences.getAccessToken()?.let { viewModel.requestReceiverHistoryInfo(it) }
+    }
+
+    private fun getReceiverHistoryInfo(){
+        viewModel.receiverHistoryInfo.observe(viewLifecycleOwner, Observer { data ->
+            binding.tvDollarNum.text = data.data!!.amount.toString()
+        })
     }
 
     private fun setViewPager(){
