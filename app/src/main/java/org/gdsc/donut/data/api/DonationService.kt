198e552af8ab5a17529db81bdf3eedf92dbe016d
hyuna <akxmcse@gmail.com>
2024-02-18 02:55:43 +0900

    [feat] donation receiver api data class 및 interface 추가

--- app/src/main/java/org/gdsc/donut/data/api/DonationService.kt
+++ app/src/main/java/org/gdsc/donut/data/api/DonationService.kt
@@ -1,4 +1,15 @@
 package org.gdsc.donut.data.api
 
+import org.gdsc.donut.data.remote.request.donation.RequestAssignReceiver
+import org.gdsc.donut.data.remote.response.donation.ResponseAssignReceiver
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+
 interface DonationService {
+    @POST("donation/receiver/assign")
+    suspend fun assignReceiver(
+        @Header("Authorization") accessToken : String,
+        @Body receiverAssignInfo: RequestAssignReceiver
+    ): ResponseAssignReceiver
 }
---
+++ app/src/main/java/org/gdsc/donut/data/remote/request/donation/RequestAssignReceiver.kt
@@ -0,0 +1,6 @@
+package org.gdsc.donut.data.remote.request.donation
+
+data class RequestAssignReceiver(
+    val store: String,
+    val price: Int
+)
