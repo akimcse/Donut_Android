b6f0c99c91219cf8300d6bcaf0bdbdbdf88e0097
hyuna <akxmcse@gmail.com>
2024-02-18 02:08:47 +0900

    [feat] ranking price api data class 및 interface 추가

--- app/src/main/java/org/gdsc/donut/data/api/RankingService.kt
+++ app/src/main/java/org/gdsc/donut/data/api/RankingService.kt
@@ -1,4 +1,12 @@
 package org.gdsc.donut.data.api
 
+import org.gdsc.donut.data.remote.response.ranking.ResponsePriceRanking
+import retrofit2.http.GET
+import retrofit2.http.Header
+
 interface RankingService {
+    @GET("history/ranking/giver/info/price")
+    suspend fun getPriceRankingInfo(
+        @Header("Authorization") accessToken : String
+    ): ResponsePriceRanking
 }
