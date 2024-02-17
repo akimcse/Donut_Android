aac0b3e8e498993a0ac22c6e9ee98cb7d7a67e29
hyuna <akxmcse@gmail.com>
2024-02-17 22:20:16 +0900

    [feat] history receiver api data class 및 interface 추가

---
+++ app/src/main/java/org/gdsc/donut/data/remote/response/home/ResponseHistoryReceiver.kt
@@ -0,0 +1,21 @@
+package org.gdsc.donut.data.remote.response.home
+
+data class ResponseHistoryReceiver(
+    val code: Int,
+    val message: String,
+    val `data`: ResponseHistoryReceiverData?
+)
+
+data class ResponseHistoryReceiverData(
+    val amount: Int,
+    val availability: Boolean,
+    val boxList: List<ResponseHistoryReceiverGift>?
+)
+
+data class ResponseHistoryReceiverGift(
+    val giftId: Long,
+    val product: String,
+    val price: Int,
+    val dueDate: String,
+    val isUsed: String
+)
