883d5d324162f4700a67cad64cbf39fad8a6437b
hyuna <akxmcse@gmail.com>
2024-02-19 05:28:45 +0900

    [chore] 로그아웃 트릭

--- app/src/main/java/org/gdsc/donut/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/org/gdsc/donut/ui/mypage/MyPageFragment.kt
@@ -1,13 +1,15 @@
 package org.gdsc.donut.ui.mypage
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import org.gdsc.donut.data.DonutSharedPreferences
 import org.gdsc.donut.databinding.FragmentMyPageBinding
-import org.gdsc.donut.ui.GiverMainActivity
+import org.gdsc.donut.ui.sign.SignActivity
 
 class MyPageFragment : Fragment() {
     private lateinit var binding: FragmentMyPageBinding
@@ -24,6 +26,12 @@ class MyPageFragment : Fragment() {
     }
 
     private fun setClickListener(){
+        binding.ivProfile.setOnClickListener {
+            DonutSharedPreferences.setAccessToken("")
+            requireActivity().supportFragmentManager.beginTransaction().remove(this).commit()
+            startActivity(Intent(context, SignActivity::class.java))
+        }
+
         binding.clProfiles.setOnClickListener {
             Toast.makeText(activity, "준비중입니다.", Toast.LENGTH_SHORT).show()
         }
