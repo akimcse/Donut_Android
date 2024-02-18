84e4c00d38ec5a5caaab5c48b6e1b63d92663427
hyuna <akxmcse@gmail.com>
2024-02-19 03:21:56 +0900

    [feat] donate api data class 및 interface 추가

--- app/src/main/java/org/gdsc/donut/data/api/DonationService.kt
+++ app/src/main/java/org/gdsc/donut/data/api/DonationService.kt
@@ -1,10 +1,15 @@
 package org.gdsc.donut.data.api
 
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import org.gdsc.donut.data.remote.request.donation.RequestAssignReceiver
 import org.gdsc.donut.data.remote.response.donation.ResponseAssignReceiver
+import org.gdsc.donut.data.remote.response.donation.ResponseDonateGiver
 import retrofit2.http.Body
 import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface DonationService {
     @POST("donation/receiver/assign")
@@ -12,4 +17,15 @@ interface DonationService {
         @Header("Authorization") accessToken : String,
         @Body receiverAssignInfo: RequestAssignReceiver
     ): ResponseAssignReceiver
+
+    @Multipart
+    @POST("donation/giver/donate")
+    suspend fun donateGiver(
+        @Header("Authorization") accessToken : String,
+        @Part giftImage: MultipartBody.Part?,
+        @Part("product") product: RequestBody,
+        @Part("price") price: Int,
+        @Part("dueDate") dueDate: RequestBody,
+        @Part("store") store: RequestBody
+    ): ResponseDonateGiver
 }
