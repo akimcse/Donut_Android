db047642714862003e4ca3d043f8043b0b1e5cc5
hyuna <akxmcse@gmail.com>
2024-02-18 13:22:27 +0900

    [feat] spinner 구현

--- app/src/main/java/org/gdsc/donut/ui/history/GiverHistoryFragment.kt
+++ app/src/main/java/org/gdsc/donut/ui/history/GiverHistoryFragment.kt
@@ -1,18 +1,20 @@
 package org.gdsc.donut.ui.history
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
-import com.google.android.material.tabs.TabLayoutMediator
+import org.gdsc.donut.R
 import org.gdsc.donut.databinding.FragmentGiverHistoryBinding
-import org.gdsc.donut.ui.GiverMainActivity
-import org.gdsc.donut.ui.history.adapter.HistoryViewPagerAdapter
 
 class GiverHistoryFragment : Fragment() {
     private lateinit var binding: FragmentGiverHistoryBinding
-    private lateinit var itemViewPagerAdapter: HistoryViewPagerAdapter
+    private var filteredYear = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,24 +22,19 @@ class GiverHistoryFragment : Fragment() {
     ): View? {
         binding = FragmentGiverHistoryBinding.inflate(inflater, container, false)
 
-        setViewPager()
-        initTabLayout()
+        setDropDownMenu()
 
         return binding.root
     }
 
-    private fun setViewPager(){
-        val fragmentList = listOf(UnusedFragment(), UsedFragment())
-
-        itemViewPagerAdapter = HistoryViewPagerAdapter(this.requireActivity())
-        itemViewPagerAdapter.fragments.addAll(fragmentList)
-
-        binding.vpItems.adapter = itemViewPagerAdapter
-    }
-
-    private fun initTabLayout(){
-        val tabLabel = listOf("unused", "used")
-        TabLayoutMediator(binding.tlMenu, binding.vpItems){tab, position -> tab.text = tabLabel[position]}.attach()
+    @SuppressLint("ResourceType")
+    private fun setDropDownMenu(){
+        val years = arrayOf("2024", "2023", "2022", "2021")
+        val yearSpinner = binding.spnYearMenu
+        val spinnerAdapter: ArrayAdapter<String>? = context?.let { ArrayAdapter(it, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, years) }
+        spinnerAdapter?.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item)
+        yearSpinner.adapter = spinnerAdapter
+        yearSpinner.onItemClickListener = AdapterView.OnItemClickListener { p0, p1, p2, p3 -> filteredYear = p0?.getItemAtPosition(p2) as String }
     }
 
     companion object {
