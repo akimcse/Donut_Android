4a0d2976b559b4445685956f050e2fa780944401
hyuna <akxmcse@gmail.com>
2024-04-23 16:09:52 +0900

    [add] add giver wallet fragment

---
+++ app/src/main/java/org/gdsc/donut/ui/wallet/GiverWalletFragment.kt
@@ -0,0 +1,24 @@
+package org.gdsc.donut.ui.wallet
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.gdsc.donut.databinding.FragmentGiverWalletBinding
+
+
+class GiverWalletFragment : Fragment() {
+    private lateinit var binding: FragmentGiverWalletBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentGiverWalletBinding.inflate(inflater, container, false)
+
+
+
+        return binding.root
+    }
+}
