442747c1a14e0689e875a6d05302b43a81f6dd8c
hyuna <akxmcse@gmail.com>
2024-04-28 21:26:08 +0900

    [feat] link to maps activity

--- app/src/main/java/org/gdsc/donut/ui/receive/ReceiveStoreStartFragment.kt
+++ app/src/main/java/org/gdsc/donut/ui/receive/ReceiveStoreStartFragment.kt
@@ -1,14 +1,21 @@
 package org.gdsc.donut.ui.receive
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import org.gdsc.donut.databinding.FragmentReceiveStoreStartBinding
 import org.gdsc.donut.ui.ReceiverMainActivity
 
-class ReceiveStoreStartFragment : Fragment() {
+class ReceiveStoreStartFragment : Fragment(){
     private lateinit var binding: FragmentReceiveStoreStartBinding
 
     override fun onCreateView(
@@ -18,8 +25,16 @@ class ReceiveStoreStartFragment : Fragment() {
         binding = FragmentReceiveStoreStartBinding.inflate(inflater, container, false)
 
         (activity as ReceiverMainActivity).enableFloatingButton()
+        setFindButton()
 
         return binding.root
+    }
+
+    private fun setFindButton(){
+        binding.btnFind.setOnClickListener {
+            startActivity(Intent(context, ReceiveMapsActivity::class.java))
+            (activity as ReceiverMainActivity).changeFragment("receive_store")
+        }
     }
 
 }
