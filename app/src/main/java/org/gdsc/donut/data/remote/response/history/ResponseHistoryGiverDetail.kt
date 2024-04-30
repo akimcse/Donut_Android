dfab1f95b7ff66883886e1cf227a5c5c80b3ccb1
hyuna <akxmcse@gmail.com>
2024-05-01 03:51:52 +0900

    [feat] update history detail API

--- app/src/main/java/org/gdsc/donut/data/remote/response/history/ResponseHistoryGiverDetail.kt
+++ app/src/main/java/org/gdsc/donut/data/remote/response/history/ResponseHistoryGiverDetail.kt
@@ -12,8 +12,10 @@ data class ResponseHistoryGiverDetailData(
     val dueDate: String,
     val store: String,
     val receiver: String,
+    val giver: String,
     val isAssigned: Boolean,
     val status: String,
     val message: String,
-    val donateDate: String
+    val donateDate: String,
+    val receivedDate: String
 )
