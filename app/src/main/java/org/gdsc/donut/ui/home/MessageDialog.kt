729c18b3cbc17d26dbeaf28466e2cbd3426a4a3f
hyuna <akxmcse@gmail.com>
2024-05-01 05:51:43 +0900

    [feat] show selected message

--- app/src/main/java/org/gdsc/donut/ui/home/MessageDialog.kt
+++ app/src/main/java/org/gdsc/donut/ui/home/MessageDialog.kt
@@ -3,6 +3,8 @@ package org.gdsc.donut.ui.home
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import org.gdsc.donut.R
 import org.gdsc.donut.databinding.DialogMessageBinding
 import org.gdsc.donut.ui.viewModel.HomeViewModel
 
@@ -31,14 +33,32 @@ class MessageDialog(context: Context, messageDialogInterface: MessageDialogInter
 
     private fun setMsg(){
         binding.clThanks.setOnClickListener {
+            binding.clThanks.setBackgroundResource(R.drawable.bg_maincoral_round8)
+            binding.tvThanks.setTextColor(ContextCompat.getColor(binding.root.context, R.color.main_coral))
+            binding.clLifesaver.setBackgroundResource(R.drawable.bg_gray300_round8)
+            binding.tvLifesaver.setTextColor(ContextCompat.getColor(binding.root.context, R.color.gray_300))
+            binding.clLove.setBackgroundResource(R.drawable.bg_gray300_round8)
+            binding.tvLove.setTextColor(ContextCompat.getColor(binding.root.context, R.color.gray_300))
             viewModel.setContent(binding.tvThanks.text.toString())
         }
 
         binding.clLifesaver.setOnClickListener {
+            binding.clThanks.setBackgroundResource(R.drawable.bg_gray300_round8)
+            binding.tvThanks.setTextColor(ContextCompat.getColor(binding.root.context, R.color.gray_300))
+            binding.clLifesaver.setBackgroundResource(R.drawable.bg_maincoral_round8)
+            binding.tvLifesaver.setTextColor(ContextCompat.getColor(binding.root.context, R.color.main_coral))
+            binding.clLove.setBackgroundResource(R.drawable.bg_gray300_round8)
+            binding.tvLove.setTextColor(ContextCompat.getColor(binding.root.context, R.color.gray_300))
             viewModel.setContent(binding.tvLifesaver.text.toString())
         }
 
         binding.clLove.setOnClickListener {
+            binding.clThanks.setBackgroundResource(R.drawable.bg_gray300_round8)
+            binding.tvThanks.setTextColor(ContextCompat.getColor(binding.root.context, R.color.gray_300))
+            binding.clLifesaver.setBackgroundResource(R.drawable.bg_gray300_round8)
+            binding.tvLifesaver.setTextColor(ContextCompat.getColor(binding.root.context, R.color.gray_300))
+            binding.clLove.setBackgroundResource(R.drawable.bg_maincoral_round8)
+            binding.tvLove.setTextColor(ContextCompat.getColor(binding.root.context, R.color.main_coral))
             viewModel.setContent(binding.tvLove.text.toString())
         }
 
