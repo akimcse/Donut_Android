14ff75c5200bcedfae608f4b638d519b0476575e
hyuna <akxmcse@gmail.com>
2024-05-01 14:00:32 +0900

    [feat] implement firebase cloud messaging on foreground

--- app/src/main/java/org/gdsc/donut/service/FirebaseMessageService.kt
+++ app/src/main/java/org/gdsc/donut/service/FirebaseMessageService.kt
@@ -13,6 +13,7 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.gdsc.donut.R
 import org.gdsc.donut.data.DonutSharedPreferences
 import org.gdsc.donut.data.api.RetrofitBuilder
 import org.gdsc.donut.data.remote.request.auth.RequestSendFCMToken
@@ -29,6 +30,42 @@ class FirebaseMessageService : FirebaseMessagingService() {
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
 
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+            sendNotification(
+                remoteMessage.data["title"].toString(),
+                remoteMessage.data["body"].toString()
+            )
+        } else {
+            remoteMessage.notification?.let {
+                sendNotification(
+                    remoteMessage.notification!!.title.toString(),
+                    remoteMessage.notification!!.body.toString()
+                )
+            }
+        }
+    }
+
+    private fun sendNotification(title: String, body: String) {
+        val channelId = "fcm_default_channel"
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_donut_app)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0, notificationBuilder.build())
     }
 }
